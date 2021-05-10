4ddca1e43ed30587510cb4472be42c19fbdb237d
swj1265 <swj1265@naver.com>
2021-05-10 15:13:59 +0900

    20210510

--- 1Week/WJ/10775.py
+++ 1Week/WJ/10775.py
@@ -1,14 +1,17 @@
 G = int(input())
 P = int(input())
-
-input = []
+ans = 1
+inputs = []
 
 for i in range(P):
     a = int(input())
     if a >= 1 and a <= G:
-        input.append(a)
+        inputs.append(a)
 
-input.sort()
-
-for i in range(input):
-
+inputs.sort()
+tmp = inputs[0]
+for i in inputs:
+    if i != tmp:
+        tmp = i
+        ans += 1
+print(ans)
