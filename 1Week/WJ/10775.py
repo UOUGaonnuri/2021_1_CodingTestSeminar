e9cce3742edb41457d666e0512fc49f2883a46db
swj1265 <swj1265@naver.com>
2021-05-15 07:27:58 +0900

    20210515

--- 1Week/WJ/10775.py
+++ 1Week/WJ/10775.py
@@ -1,17 +1,30 @@
-G = int(input())
-P = int(input())
-ans = 1
-inputs = []
+import sys
 
-for i in range(P):
-    a = int(input())
-    if a >= 1 and a <= G:
-        inputs.append(a)
+G = int(sys.stdin.readline().rstrip("\n"))
+P = int(sys.stdin.readline().rstrip("\n"))
+plist = [int(sys.stdin.readline().rstrip("\n")) for i in range (0,P)]
 
-inputs.sort()
-tmp = inputs[0]
-for i in inputs:
-    if i != tmp:
-        tmp = i
-        ans += 1
-print(ans)
+def findParent(x):
+   if parent[x] == x:
+       return x
+   p = findParent(parent[x])
+   parent[x]=p
+   return parent[x]
+
+def unionParent(x,y):
+    x = findParent(x)
+    y = findParent(y)
+    if x<y:
+        parent[y] = x
+    else:
+        parent[x] = y
+# 이중 반복문으로는 시간 초과 => union-find이용
+parent = {i:i for i in range(0,G+1)}
+count=0
+for plist in plists:
+    x = findParent(plist)
+    if x==0:
+        break
+    unionParent(x,x-1)
+    count+=1
+print(count)
